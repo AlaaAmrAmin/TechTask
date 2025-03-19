8c19f65b984f0e077f894bf39ab014665c02589a
Alaa Amin <alaaamin@skyscanner.net>
2025-03-19 17:23:57 +0000

    Initial Commit

---
+++ Rightmove/RightmoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightmoveApp.swift
+//  Rightmove
+//
+//  Created by Alaa Amin on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RightmoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
